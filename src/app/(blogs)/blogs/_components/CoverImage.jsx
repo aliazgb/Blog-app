3886a9f0e67ef6667e437cc952cddcd79363bb37
aliazgb <ali.azgb10@gmail.com>
2025-08-31 19:39:20 +0200

    test for deploy

--- src/app/(blogs)/blogs/_components/CoverImage.jsx
+++ src/app/(blogs)/blogs/_components/CoverImage.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
-function CoverImage({ coverImageUrl, slug, title }) {
+function CoverImage({ coverImage, slug, title }) {
   return (
     <div className="relative aspect-w-16 aspect-h-9 overflow-hidden rounded-lg mb-6">
       <Image
         className="object-cover object-center hover:scale-110 transition-all ease-out duration-300"
         fill
-        src={coverImageUrl}
+        src={coverImage}
         alt={title}
       />
     </div>
