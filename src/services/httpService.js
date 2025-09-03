468f9e7726ab9736fdbcae756b2140b2a9f0f74a
aliazgb <ali.azgb10@gmail.com>
2025-09-03 14:42:35 +0200

    test

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 
 const app = axios.create({
-  baseURL: "api.blog-app.online",
+  baseURL: "api.blog-app.online/api",
   withCredentials: true,
 });
 
@@ -30,7 +30,7 @@ app.interceptors.response.use(
       originalConfig._retry = true;
       try {
         const { data } = await axios.get(
-          `api.blog-app.online/user/refresh-token`,
+          `api.blog-app.online/api/user/refresh-token`,
           {
             withCredentials: true,
           }
